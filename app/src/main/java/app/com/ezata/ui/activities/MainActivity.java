f1808f93baf173914be8d2dddb34ba80493bd68f
Ali <muhammadali90995@gmail.com>
2022-04-28 17:32:58 +0500

    Manage States

--- app/src/main/java/app/com/ezata/ui/activities/MainActivity.java
+++ app/src/main/java/app/com/ezata/ui/activities/MainActivity.java
@@ -3,6 +3,7 @@ package app.com.ezata.ui.activities;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,13 +33,18 @@ import app.com.ezata.ui.fragments.OrderDetailFragment;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     ActivityMainBinding binding;
-    int state[] = {R.drawable.ic_active, R.drawable.ic_pause, R.drawable.ic_inactive};
+    int[] state = {R.drawable.ic_active, R.drawable.ic_pause, R.drawable.ic_inactive};
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    String State = "s", text, paused = "2", pauseText = "2";
+    ImageView ivActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        ivActive = binding.drawer.findViewById(R.id.ivActive);
         binding.inprogress.setVisibility(View.GONE);
         binding.ivPrepareFoodSelect.setVisibility(View.VISIBLE);
         binding.ivPrepareOrder.setVisibility(View.GONE);
@@ -46,6 +53,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             binding.drawer.findViewById(R.id.layoutNavigationItems).setVisibility(View.GONE);
             binding.drawer.findViewById(R.id.layouStatus).setVisibility(View.VISIBLE);
         });
+
+        manageStateClick();
+        loadData();
+        updateViews();
+
         binding.ivHistory.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), HistoryActivity.class)));
         binding.ivMenu.setOnClickListener(view -> {
             openNavDrawer();
@@ -90,6 +102,154 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         });
     }
 
+    private void manageStateClick() {
+        binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeLayout).setOnClickListener(view -> {
+            ivActive.setImageResource(state[0]);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.VISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.INVISIBLE);
+            activeState();
+        });
+        binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseLayout).setOnClickListener(view -> {
+            ivActive.setImageResource(state[1]);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
+            pauseState();
+        });
+
+        binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                .findViewById(R.id.tv15M).setOnClickListener(view1 -> {
+            ivActive.setImageResource(state[1]);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv15M).setBackgroundResource(R.drawable.ic_black_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv30M).setBackgroundResource(R.drawable.ic_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv1H).setBackgroundResource(R.drawable.ic_circle);
+            pauseText = "1";
+            pauseState();
+        });
+
+        binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                .findViewById(R.id.tv30M).setOnClickListener(view1 -> {
+            ivActive.setImageResource(state[1]);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv15M).setBackgroundResource(R.drawable.ic_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv30M).setBackgroundResource(R.drawable.ic_black_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv1H).setBackgroundResource(R.drawable.ic_circle);
+            pauseText = "2";
+            pauseState();
+        });
+
+        binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                .findViewById(R.id.tv1H).setOnClickListener(view1 -> {
+            ivActive.setImageResource(state[1]);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv15M).setBackgroundResource(R.drawable.ic_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv30M).setBackgroundResource(R.drawable.ic_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv1H).setBackgroundResource(R.drawable.ic_black_circle);
+            pauseText = "3";
+            pauseState();
+        });
+
+        binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveLayout).setOnClickListener(view -> {
+            ivActive.setImageResource(state[2]);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.VISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.INVISIBLE);
+            inactiveState();
+        });
+    }
+
+    private void activeState() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(State, "Active");
+        editor.apply();
+    }
+
+    private void pauseState() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(State, "Pause");
+        editor.putString(paused, pauseText);
+        editor.apply();
+    }
+
+
+    private void inactiveState() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(State, "Inactive");
+        editor.apply();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedPreferences.getString(State, "Active");
+        pauseText = sharedPreferences.getString(paused, pauseText);
+    }
+
+    public void updateViews() {
+        if (text.equals("Active")) {
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.VISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.INVISIBLE);
+            ivActive.setImageResource(state[0]);
+        }
+        if (text.equals("Pause")) {
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
+            ivActive.setImageResource(state[1]);
+        }
+
+        if (text.equals("Inactive")) {
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.VISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.INVISIBLE);
+            ivActive.setImageResource(state[2]);
+        }
+        if (pauseText.equals("1")) {
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv15M).setBackgroundResource(R.drawable.ic_black_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv30M).setBackgroundResource(R.drawable.ic_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv1H).setBackgroundResource(R.drawable.ic_circle);
+        }
+        if (pauseText.equals("2")) {
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv15M).setBackgroundResource(R.drawable.ic_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv30M).setBackgroundResource(R.drawable.ic_black_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv1H).setBackgroundResource(R.drawable.ic_circle);
+        }
+        if (pauseText.equals("3")) {
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv15M).setBackgroundResource(R.drawable.ic_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv30M).setBackgroundResource(R.drawable.ic_circle);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
+                    .findViewById(R.id.tv1H).setBackgroundResource(R.drawable.ic_black_circle);
+        }
+    }
+
     private void openNavDrawer() {
         binding.drawer.openDrawer(Gravity.LEFT);
     }
@@ -114,8 +274,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ft.replace(R.id.framelayout1, new InProgressOrderFragment());
         ft.replace(R.id.framelayout, new InProgressOrderDetailsFragment());
         ft.commit();
-
-
     }
 
     private void setOrderDetails() {
