a05bbbce34e927c57652a5bf7cf828afebc89687
Ali <muhammadali90995@gmail.com>
2022-04-30 01:05:41 +0500

    Manage States

--- app/src/main/java/app/com/ezata/ui/activities/MainActivity.java
+++ app/src/main/java/app/com/ezata/ui/activities/MainActivity.java
@@ -7,6 +7,11 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -16,11 +21,17 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import app.com.ezata.R;
 import app.com.ezata.adapter.CustomAdapter;
@@ -50,11 +61,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         binding.ivPrepareOrder.setVisibility(View.GONE);
 
         binding.drawer.findViewById(R.id.ivActive).setOnClickListener(v -> {
-            if (binding.drawer.findViewById(R.id.layoutNavigationItems).getVisibility() == View.VISIBLE){
+            if (binding.drawer.findViewById(R.id.layoutNavigationItems).getVisibility() == View.VISIBLE) {
                 binding.drawer.findViewById(R.id.layoutNavigationItems).setVisibility(View.GONE);
                 binding.drawer.findViewById(R.id.layouStatus).setVisibility(View.VISIBLE);
-            }
-           else {
+            } else {
                 binding.drawer.findViewById(R.id.layoutNavigationItems).setVisibility(View.VISIBLE);
                 binding.drawer.findViewById(R.id.layouStatus).setVisibility(View.GONE);
             }
@@ -68,6 +78,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         binding.ivMenu.setOnClickListener(view -> {
             openNavDrawer();
         });
+        checkInternetStatus();
         setNewOrderBgColor();
         setOrderFragment();
         setOrderDetails();
@@ -108,29 +119,103 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         });
     }
 
+    private void checkInternetStatus() {
+
+        if (isConnected(getApplicationContext())) {
+
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"yes Internet Connection", Snackbar.LENGTH_LONG);
+            snackbar.setBackgroundTint(Color.RED);
+            snackbar.show();
+        } else {
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"No Internet Connection", Snackbar.LENGTH_LONG);
+            snackbar.setBackgroundTint(Color.RED);
+            snackbar.show();
+        }
+    }
+
+    public boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+//        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+//        if(!(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED) ||
+//                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+//
+//
+//        }
+
+
+    public boolean isConnectedTo() {
+        ConnectivityManager cm = (ConnectivityManager) getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) {
+            return false;
+        }
+        /* NetworkInfo is deprecated in API 29 so we have to check separately for higher API Levels */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            Network network = cm.getActiveNetwork();
+            if (network == null) {
+                return false;
+            }
+            NetworkCapabilities networkCapabilities = cm.getNetworkCapabilities(network);
+            if (networkCapabilities == null) {
+                return false;
+            }
+            boolean isInternetSuspended = !networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED);
+            return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                    && !isInternetSuspended;
+        } else {
+            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isConnected();
+        }
+    }
+
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress address = InetAddress.getByName("www.google.com");
+            return !address.equals("");
+        } catch (UnknownHostException e) {
+            // Log error
+        }
+        return false;
+    }
+
     private void manageStateClick() {
         binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeLayout).setOnClickListener(view -> {
             ivActive.setImageResource(state[0]);
+            binding.ivMenu.setImageResource(R.drawable.ic_menu_active);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.VISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layoutNavigationItems).setVisibility(View.VISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout).setVisibility(View.GONE);
             activeState();
         });
         binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseLayout).setOnClickListener(view -> {
             ivActive.setImageResource(state[1]);
+            binding.ivMenu.setImageResource(R.drawable.ic_menu_paused);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
-            binding.drawer.findViewById(R.id.layoutNavigationItems).setVisibility(View.VISIBLE);
-            binding.drawer.findViewById(R.id.layouStatus).setVisibility(View.INVISIBLE);
+//            binding.drawer.findViewById(R.id.layoutNavigationItems).setVisibility(View.VISIBLE);
+//           binding.drawer.findViewById(R.id.layouStatus).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout).setVisibility(View.VISIBLE);
             pauseState();
         });
 
         binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
                 .findViewById(R.id.tv15M).setOnClickListener(view1 -> {
             ivActive.setImageResource(state[1]);
+            binding.ivMenu.setImageResource(R.drawable.ic_menu_paused);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
@@ -149,6 +234,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
                 .findViewById(R.id.tv30M).setOnClickListener(view1 -> {
             ivActive.setImageResource(state[1]);
+            binding.ivMenu.setImageResource(R.drawable.ic_menu_paused);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
@@ -167,6 +253,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout)
                 .findViewById(R.id.tv1H).setOnClickListener(view1 -> {
             ivActive.setImageResource(state[1]);
+            binding.ivMenu.setImageResource(R.drawable.ic_menu_paused);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.VISIBLE);
@@ -184,11 +271,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveLayout).setOnClickListener(view -> {
             ivActive.setImageResource(state[2]);
+            binding.ivMenu.setImageResource(R.drawable.ic_menu_inactive);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.activeCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.inactiveCheck).setVisibility(View.VISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.pauseCheck).setVisibility(View.INVISIBLE);
             binding.drawer.findViewById(R.id.layoutNavigationItems).setVisibility(View.VISIBLE);
             binding.drawer.findViewById(R.id.layouStatus).setVisibility(View.INVISIBLE);
+            binding.drawer.findViewById(R.id.layouStatus).findViewById(R.id.timerLayout).setVisibility(View.GONE);
             inactiveState();
         });
     }
